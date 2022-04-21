c2fada6b98812de36b189108924147d5673193e0
Victor Jerez <vjerezm@miumg.edu.gt>
2022-04-21 14:35:41 -0600

    Se agregan unos comentarios

--- PrototipoP12P2022/include/persona.h
+++ PrototipoP12P2022/include/persona.h
@@ -12,32 +12,32 @@ class persona
         void login();
         void MenuGeneral();
         void catalogos();
-        void entrenador();
+        void entrenador();////Metodos para CRUD entrenador
         void altas1();
         void bajas1();
         void modificaciones1();
         void lectura1();
-        void equipo();
+        void equipo();////Metodos para CRUD Equipos
         void altas2();
         void bajas2();
         void modificaciones2();
         void lectura2();
-        void pais();
+        void pais();////Metodos para CRUD pais
         void altas3();
         void bajas3();
         void modificaciones3();
         void lectura3();
-        void puesto();
+        void puesto();////Metodos para CRUD puestos
         void altas4();
         void bajas4();
         void modificaciones4();
         void lectura4();
-        void jugador();
+        void jugador();////Metodos para CRUD jugador
         void altas5();
         void bajas5();
         void modificaciones5();
         void lectura5();
-        void informes();
+        void informes(); ////Metodos para la parte de informes
         void infoEntrenador();
         void infoEquipo();
         void infopais();
